fd2caf04f611487895d67105dfd5d437a6821e71
Christian Agner <christianagnerzxc@gmail.com>
2022-03-27 21:11:48 +0800

    update files, added pages, first commit

---
+++ activity-1/src/app/pages/firstnav/firstnav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firstnav',
+  templateUrl: './firstnav.component.html',
+  styleUrls: ['./firstnav.component.css']
+})
+export class FirstnavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
