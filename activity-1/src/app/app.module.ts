8481f504db90734a51375da55c352f54435bf68b
Christian Agner <christianagnerzxc@gmail.com>
2022-03-28 00:04:22 +0800

    added first nav content

--- activity-1/src/app/app.module.ts
+++ activity-1/src/app/app.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FirstnavComponent } from './pages/firstnav/firstnav.component';
 import { SecondnavComponent } from './pages/secondnav/secondnav.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+
+
+const routes: Routes = [
+  {path: "firstnav", component: FirstnavComponent,
+    children: [
+    {path: "secondnav", component: SecondnavComponent},
+    ]
+  },
+  {path: "**", redirectTo: "firstnav"}
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +28,11 @@ import { SecondnavComponent } from './pages/secondnav/secondnav.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- activity-1/src/app/pages/firstnav/firstnav.component.ts
+++ activity-1/src/app/pages/firstnav/firstnav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-firstnav',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./firstnav.component.css']
 })
 export class FirstnavComponent implements OnInit {
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+
 
   constructor() { }
 
